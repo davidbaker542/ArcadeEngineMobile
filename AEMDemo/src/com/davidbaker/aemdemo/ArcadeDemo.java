8c3d8c133440e63c187587d71b2573b3f217cf6e
davidbaker542 <guitarplayer19997@gmail.com>
2014-09-05 15:03:23 -0400

    Init Commit

---
+++ AEMDemo/src/com/davidbaker/aemdemo/ArcadeDemo.java
@@ -0,0 +1,18 @@
+package com.davidbaker.aemdemo;
+
+import com.aem.src.AnimActivity;
+import com.aem.src.Graphics;
+
+public class ArcadeDemo extends AnimActivity {
+
+	@Override
+	public void renderFrame(Graphics g) {
+		
+		
+	}
+
+	@Override
+	public void process() {
+
+	}
+}
